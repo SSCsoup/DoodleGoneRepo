f92402d5d224e5c3a41d67ee49f445a4e54fa386
vikasreddy2000 <w0874034@myscc.ca>
2025-02-20 21:28:29 -0500

    created about us page

--- DoodleGoneWeb/obj/Debug/net8.0/DoodleGoneWeb.AssemblyInfo.cs
+++ DoodleGoneWeb/obj/Debug/net8.0/DoodleGoneWeb.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoodleGoneWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6e7d6e46aba5bd511c30a03c127f0f550d1b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e9338c0c8f588e526fd4d549d77e81f601de85")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoodleGoneWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoodleGoneWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

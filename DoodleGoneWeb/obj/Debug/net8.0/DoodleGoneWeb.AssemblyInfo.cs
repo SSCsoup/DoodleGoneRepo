b3e9338c0c8f588e526fd4d549d77e81f601de85
SSCsoup <w0517870@myscc.ca>
2025-02-20 21:12:16 -0500

    created the rest of the files and fixed css issue

--- DoodleGoneWeb/obj/Debug/net8.0/DoodleGoneWeb.AssemblyInfo.cs
+++ DoodleGoneWeb/obj/Debug/net8.0/DoodleGoneWeb.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoodleGoneWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a8cfed376bf711f69243fae56f3e889f6e4900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6e7d6e46aba5bd511c30a03c127f0f550d1b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoodleGoneWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoodleGoneWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
